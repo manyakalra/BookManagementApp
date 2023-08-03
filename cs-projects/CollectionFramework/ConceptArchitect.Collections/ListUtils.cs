4ccef973354aa009979239e93c9ce2b6935fc7cc
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2023-08-03 17:33:11 +0530

    day#14 update#3

---
+++ cs-projects/CollectionFramework/ConceptArchitect.Collections/ListUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptArchitect.Collections
+{
+    public static  class ListUtils
+    {
+        public static IIndexedList<O> Transform<I,O>(this IIndexedList<I> list, Func<I,O> converter)
+        {
+            var result = new LinkedList<O>();
+
+            for(int i=0; i<list.Length;i++)
+                result.Add(converter(list[i]));
+
+            return result;
+        }
+
+        public static void ForEach<I>(this IIndexedList<I> list, Action<I> action)
+        {
+            list.ForEach((x, i) => action(x));
+        }
+
+        public static void ForEach<I>(this IIndexedList<I> list, Action<I,int> action)
+        {
+            for (int i = 0; i < list.Length; i++)
+                action(list[i],i);
+        }
+
+    }
+}
