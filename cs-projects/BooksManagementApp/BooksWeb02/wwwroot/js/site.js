08d896c879f01d522574690f0a403930e21b8144
A M <akhilesh.am@ecolab.com>
2023-08-24 10:31:27 +0530

    Added navbar styling

--- cs-projects/BooksManagementApp/BooksWeb02/wwwroot/js/site.js
+++ cs-projects/BooksManagementApp/BooksWeb02/wwwroot/js/site.js
@@ -4,8 +4,8 @@
 // Write your JavaScript code.
 function confirmDelete() {
     console.log("We are here");
-    if (confirm("Are you sure you want to delete this record?")
+    if (confirm("Are you sure you want to delete this record?"))
     {
         window.Location.href = '@Url.Action("Delete", "Author", new {id = Model.Id})';
-    })
+    }
 }
