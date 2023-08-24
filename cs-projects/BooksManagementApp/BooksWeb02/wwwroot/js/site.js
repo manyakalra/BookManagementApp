a663253f06cb9f7ea3d43d1e182d78a2c32965cd
kalrama <kalrama@IN-8840LS3>
2023-08-24 16:12:59 +0530

    Updated Delete Working NowCSS Styling changes

--- cs-projects/BooksManagementApp/BooksWeb02/wwwroot/js/site.js
+++ cs-projects/BooksManagementApp/BooksWeb02/wwwroot/js/site.js
@@ -2,10 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function confirmDelete() {
-    console.log("We are here");
-    if (confirm("Are you sure you want to delete this record?"))
-    {
-        window.Location.href = '@Url.Action("Delete", "Author", new {id = Model.Id})';
+
+function deleteCard() {
+    // Display a confirmation dialog box
+    var result = window.confirm("Are you sure you want to delete the card?");
+
+    if (result) {
+        // If the user clicked "OK" in the dialog box
+        // Here is where you would place the actual logic to delete the card.
+        // This could involve making an API request, modifying the DOM, or other actions.
+
+        // For this example, let's assume we have a card element with an ID "cardToDelete"
+        var cardToDelete = document.getElementById("cardToDelete");
+        if (cardToDelete) {
+            cardToDelete.style.display = "none"; // Hide the card
+            confirm("Card deleted!");
+        } 
+    } else {
+        // If the user clicked "Cancel" in the dialog box
+        var cardToShow = document.getElementById("cardToDelete");
+        if (cardToShow) {
+            cardToShow.style.display = "block"; // Show the card
+            confirm("Card not deleted.");
+        } 
     }
 }
